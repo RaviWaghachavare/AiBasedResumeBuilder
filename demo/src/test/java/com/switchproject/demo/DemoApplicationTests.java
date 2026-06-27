9f0c8886d8359f3539ccc0b238d0842bb5c0f8c7
RaviWaghachaware <wravirajh@gmail.com>
2026-06-27 23:53:27 +0530

    demo peoject folder uploaded

---
+++ demo/src/test/java/com/switchproject/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.switchproject.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
